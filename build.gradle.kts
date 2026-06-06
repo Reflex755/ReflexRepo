03aeb16318fb9387dc3ac164eb4f19824f24d8c5
Reflex13371 <291193830+Reflex13371@users.noreply.github.com>
2026-06-05 19:41:00 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        // Cloudstream gradle plugin which makes everything work and builds plugins
-        classpath("com.github.recloudstream.gradle:gradle:81b1d424d")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
+        classpath("com.android.tools.build:gradle:9.1.0")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.20")
     }
 }
 
